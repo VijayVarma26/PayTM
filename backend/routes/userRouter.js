a1a573f9f2ce6591cb7f84a905d7ce3545d4ab8a
VijayVarma26 <varmavijay2021@gmail.com>
2024-08-26 22:01:18 +0530

    added /bulkroute to get all the user using search parameter

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -94,8 +94,32 @@ router.put("/", authMiddleware,async (req,res) =>{
     await User.updateOne(req.body, {
         id: req.userId
     })
-    req.json({
+    res.json({
         message: "Updated Successfully"
+    })
+})
+
+
+router.get("/bulk", async (req,res) =>{
+    const filter = req.query.filter || "" ;
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex" : filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
     })
 })
 module.exports = router
