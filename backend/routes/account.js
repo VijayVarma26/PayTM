f5f25d144c0d77b3b854909b6b1c9f3dfd4959bb
VijayVarma26 <varmavijay2021@gmail.com>
2024-08-29 21:42:37 +0530

    created account router for bank

---
+++ backend/routes/account.js
@@ -0,0 +1,17 @@
+// backend/routes/account.js
+const express = require('express');
+const { authMiddleware } = require('../middleware');
+const { Account } = require('../db');
+const { default: mongoose } = require('mongoose');
+
+const router = express.Router();
+
+router.get("/balance", authMiddleware, async (req, res) => {
+    const account = await Account.findOne({
+        userId: req.userId
+    });
+
+    res.json({
+        balance: account.balance
+    })
+});
